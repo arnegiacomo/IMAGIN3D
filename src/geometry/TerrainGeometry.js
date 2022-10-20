186f4bd2473b7a9d796fd79067f93871f819049e
Arne Munthe-Kaas <45269747+h593267@users.noreply.github.com>
2022-10-20 14:29:03 +0200

    add dat.gui (non vr), add three-mesh-ui for vr gui

---
+++ src/geometry/TerrainGeometry.js
@@ -0,0 +1,18 @@
+"use strict";
+
+import * as THREE from "../../libs/three.module.js";
+import { getHeightmapData } from "../utils.js";
+
+export default class TerrainGeometry extends THREE.PlaneGeometry {
+    constructor(size, resolution, height, image) {
+        super(size, size, resolution - 1, resolution - 1);
+
+        this.rotateX((Math.PI / 180) * -90);
+
+        const data = getHeightmapData(image, resolution);
+
+        for (let i = 0; i < data.length; i++) {
+            this.attributes.position.setY(i, data[i] * height);
+        }
+    }
+}
--- src/objects/SkyBox.js
+++ src/objects/SkyBox.js
@@ -7,12 +7,12 @@ export default class SkyBox extends THREE.CubeTexture {
     constructor() {
         const loader = new THREE.CubeTextureLoader();
         const texture = loader.load([
-            '../../public/assets/images/skybox/right.png',
-            '../../public/assets/images/skybox/left.png',
-            '../../public/assets/images/skybox/top.png',
-            '../../public/assets/images/skybox/bottom.png',
-            '../../public/assets/images/skybox/front.png',
-            '../../public/assets/images/skybox/back.png',
+            '../../assets/images/skybox/right.png',
+            '../../assets/images/skybox/left.png',
+            '../../assets/images/skybox/top.png',
+            '../../assets/images/skybox/bottom.png',
+            '../../assets/images/skybox/front.png',
+            '../../assets/images/skybox/back.png',
         ]);
         super(texture);
     }
